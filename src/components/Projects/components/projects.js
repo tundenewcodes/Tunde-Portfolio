383880fa5bb70fab3e2766bfca5bef8d1da00fda
tundenewcodes <tundef@eraconneckt.com>
2023-08-13 03:46:21 +0100

    links added

--- src/components/Projects/components/projects.js
+++ src/components/Projects/components/projects.js
@@ -1,25 +1,32 @@
 export const projects =[
     {
-    id:1,
-    technologies:"ReactJs | Typescript | TailwindCss",
+    id:0,
+    technologies:"React | Typescript | TailwindCss | Vite",
     type:"FrontEnd",
-    projectImages:["https://i.ibb.co/F3hq3xd/contact.png", "https://res.cloudinary.com/bacf/image/upload/v1690999836/Era-tek/Imag_cover_18_xtmnmr.png", "https://res.cloudinary.com/bacf/image/upload/v1689270036/Era-tek/Imag_cover_11_knkxhu.png"],
-    description:"This is a simple, easy-to-use, easy-to-navigate business landing page. Got inspiration from a local gym store in my community. It can be reused as landing page for any business",
-    github:"",
-    hostPlatform:"",
-    projectName:"Gym Landing Page"
+    projectImages:[
+        "https://i.ibb.co/35KrJVW/f1.png", 
+        "https://i.ibb.co/PrwZBc7/f2.png",
+        "https://i.ibb.co/z7kWDr3/f3.png", 
+        "https://i.ibb.co/brdSSCc/f4.png"],
+    description:"This is a landing page i used to improve my Typescript, TailwindCss and React skills. First project i build with Vite",
+    github:"https://github.com/tundenewcodes/gym-blog-app",
+    hostPlatform:"https://fitness-fam.onrender.com/",
+    projectName:"Fitness Fam Landing Page"
 }, {
     id:1,
-    technologies:"ReactJs | Typescript | TailwindCss",
+    technologies:"ReactJs | Javascript | Bootstrap",
     type:"FrontEnd",
-    projectImages:["https://i.ibb.co/F3hq3xd/contact.png", "https://res.cloudinary.com/bacf/image/upload/v1690999836/Era-tek/Imag_cover_18_xtmnmr.png", "https://res.cloudinary.com/bacf/image/upload/v1689270036/Era-tek/Imag_cover_11_knkxhu.png"],
-    description:"This is a simple, easy-to-use, easy-to-navigate business landing page. Got inspiration from a local gym store in my community. It can be reused as landing page for any business",
-    github:"",
-    hostPlatform:"",
-    projectName:"Gym Landing Page"
+    projectImages:[
+        "https://i.ibb.co/P9dhhBQ/pp.png",
+         "https://i.ibb.co/GvPFMpG/ps.png", 
+         "https://i.ibb.co/hB637gk/pd.png", "https://i.ibb.co/SKrStbP/pc.png"],
+    description:"This is my portfolio website. It contains every of my details and has great user interface",
+    github:"https://github.com/tundenewcodes/Tunde-Portfolio",
+    hostPlatform:"https://tunde-dev.onrender.com/",
+    projectName:"Portfolio landing Page"
 },
 {
-    id:1,
+    id:2,
     technologies:"ReactJs | Typescript | TailwindCss",
     type:"FrontEnd",
     projectImages:["https://i.ibb.co/F3hq3xd/contact.png", "https://res.cloudinary.com/bacf/image/upload/v1690999836/Era-tek/Imag_cover_18_xtmnmr.png", "https://res.cloudinary.com/bacf/image/upload/v1689270036/Era-tek/Imag_cover_11_knkxhu.png"],
